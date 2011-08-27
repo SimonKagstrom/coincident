e9a57186f8e2de226a3c96c8b24cd12f94f52423
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-08-27 09:40:05 +0200

    unit test: Add expectations and tests for setting function breakpoints
    Provides much better test coverage.

--- test/tests-elf.cc
+++ test/tests-elf.cc
@@ -6,14 +6,11 @@
 
 #include <string>
 
+static MockPtrace ptraceInstance;
+
 IPtrace &IPtrace::getInstance()
 {
-	static IPtrace *instance;
-
-	if (!instance)
-		instance = new MockPtrace();
-
-	return *instance;
+	return ptraceInstance;
 }
 
 class FunctionListener : public IElf::IFunctionListener
@@ -27,12 +24,46 @@ public:
 			ASSERT_TRUE(v >= 0);
 
 			std::list<int> list = fn.setupMemoryBreakpoints();
-			// Can't read data with ptrace now...
-			ASSERT_TRUE(list.size() == 0);
+			if (strcmp(fn.getName(), "vobb") == 0)
+				ASSERT_TRUE(list.size() > 0);
 		}
 
 		std::map<std::string, int> m_map;
 };
+
+static uint8_t asm_dump[] =
+{
+		0x76, 0x27,       //                   jbe    804ab40 <ud_decode+0x630>
+		0x89, 0x1c, 0x24, //                   mov    %ebx,(%esp)
+		0x83, 0xe0, 0x0f, //                   and    $0xf,%eax
+		0x8d, 0x50, 0x01, //                   lea    0x1(%eax),%edx
+		0xd1, 0xea,       //                   shr    %edx
+		0x88, 0x55, 0xcc, //                   mov    %dl,-0x34(%ebp)
+		0xe8, 0x88, 0xe3, 0xff, 0xff, //       call   8048ef0 <inp_next>
+		0x8b, 0x83, 0x58, 0x02, 0x00, 0x00, // mov    0x258(%ebx),%eax
+};
+
+
+bool mockReadMemory(uint8_t *dst, void *start, size_t bytes)
+{
+	size_t to_cpy = bytes;
+
+	if (to_cpy < sizeof(asm_dump))
+		to_cpy = sizeof(asm_dump);
+
+	memcpy(dst, asm_dump, to_cpy);
+
+	return true;
+}
+
+int mockSetBreakpoint(void *addr)
+{
+	static int id = 0;
+
+	id++;
+
+	return id;
+}
 
 TEST(elffile)
 {
@@ -48,6 +79,11 @@ TEST(elffile)
 	res = elf.setFile(&listener, filename);
 	ASSERT_TRUE(res == false);
 
+	EXPECT_CALL(ptraceInstance, setBreakpoint(_))
+		.Times(AtLeast(1))
+		.WillRepeatedly(Invoke(mockSetBreakpoint));
+	EXPECT_CALL(ptraceInstance, readMemory(_, _,_))
+		.WillRepeatedly(Invoke(mockReadMemory));
 
 	// Don't run the unit test as root!
 	sprintf(filename, "%s/test-elf", crpcut::get_start_dir());
