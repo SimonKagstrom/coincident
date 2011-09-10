f586bfae3c29af17d54ed2a54bea707f8d25237a
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-09-10 14:46:50 +0200

    unit test: Add tests for the controller

---
+++ test/tests-controller.cc
@@ -0,0 +1,57 @@
+#include "test.hh"
+
+#include <controller.hh>
+#include "mock-ptrace.hh"
+
+static int test_thread(void *priv)
+{
+	return 0;
+}
+
+TEST(controllerForkError)
+{
+	IController &controller = IController::getInstance();
+	MockPtrace &ptrace = (MockPtrace &)IPtrace::getInstance();
+
+	EXPECT_CALL(ptrace, forkAndAttach())
+		.Times(AtLeast(1))
+		.WillRepeatedly(Return(-1));
+
+	controller.run();
+}
+
+TEST(controllerAddProcessForkError)
+{
+	IController &controller = IController::getInstance();
+	MockPtrace &ptrace = (MockPtrace &)IPtrace::getInstance();
+
+	EXPECT_CALL(ptrace, forkAndAttach())
+		.Times(AtLeast(1))
+		.WillRepeatedly(Return(-1));
+
+	controller.addThread(test_thread, NULL);
+	controller.run();
+}
+
+
+TEST(controllerRunChild)
+{
+	IController &controller = IController::getInstance();
+	MockPtrace &ptrace = (MockPtrace &)IPtrace::getInstance();
+
+	EXPECT_CALL(ptrace, forkAndAttach())
+		.Times(AtLeast(1))
+		.WillRepeatedly(Return(100));
+
+	IPtrace::PtraceEvent ev;
+
+	ev.type = ptrace_breakpoint;
+	ev.eventId = 100;
+	ev.addr = NULL;
+	EXPECT_CALL(ptrace, continueExecution(_))
+		.Times(AtLeast(1))
+		.WillRepeatedly(Return(ev));
+
+	controller.addThread(test_thread, NULL);
+	controller.run();
+}
