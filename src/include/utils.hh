b774b398c13222e37295ac6b75545f9135323b3e
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-09-11 08:34:19 +0200

    utils: Add xmalloc

--- src/include/utils.hh
+++ src/include/utils.hh
@@ -33,3 +33,13 @@ static inline char *xstrdup(const char *s)
 
 	return out;
 }
+
+static inline void *xmalloc(size_t sz)
+{
+  void *out = malloc(sz);
+
+  panic_if(!out, "malloc failed");
+  memset(out, 0, sz);
+
+  return out;
+}
