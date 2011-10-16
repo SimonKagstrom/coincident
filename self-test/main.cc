65150b1e3f558ab01d4b86a8c9a957c55d9de722
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-10-16 20:46:41 +0200

    self-test: Add small live-system self test

---
+++ self-test/main.cc
@@ -0,0 +1,25 @@
+#include <controller.hh>
+#include <stdio.h>
+
+static int fn_a(void *params)
+{
+	printf("fn_a!\n");
+	return 1;
+}
+
+static int fn_b(void *params)
+{
+	printf("fn_b!\n");
+	return 2;
+}
+
+int main(int argc, const char *argv[])
+{
+	IController &controller = IController::getInstance();
+
+	controller.addThread(fn_a, NULL);
+	controller.addThread(fn_b, NULL);
+	controller.run();
+
+	return 0;
+}
