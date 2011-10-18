fcec89473a29e60bc5b7d3c3808053e90cd1f012
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-10-18 18:23:18 +0200

    unit test: Test disassembly offsets as well

--- test/tests-disassembly.cc
+++ test/tests-disassembly.cc
@@ -19,16 +19,21 @@ public:
 
 	void onMemoryReference(off_t offset, bool isLoad)
 	{
+		bool off_ok = offset == 2 || offset == 8 || offset == 13 || offset == 21;
+
+		ASSERT_TRUE(off_ok);
 		m_memRefs++;
 	}
 
 	void onCall(off_t offset)
 	{
+		ASSERT_TRUE(offset == 16);
 		m_calls++;
 	}
 
 	void onBranch(off_t offset)
 	{
+		ASSERT_TRUE(offset == 0);
 		m_branches++;
 	}
 
@@ -41,14 +46,14 @@ public:
 
 static uint8_t asm_dump[] =
 {
-		0x76, 0x27,       //                   jbe    804ab40 <ud_decode+0x630>
-		0x89, 0x1c, 0x24, //                   mov    %ebx,(%esp)
-		0x83, 0xe0, 0x0f, //                   and    $0xf,%eax
-		0x8d, 0x50, 0x01, //                   lea    0x1(%eax),%edx
-		0xd1, 0xea,       //                   shr    %edx
-		0x88, 0x55, 0xcc, //                   mov    %dl,-0x34(%ebp)
-		0xe8, 0x88, 0xe3, 0xff, 0xff, //       call   8048ef0 <inp_next>
-		0x8b, 0x83, 0x58, 0x02, 0x00, 0x00, // mov    0x258(%ebx),%eax
+		0x76, 0x27,       //                    0 jbe    804ab40 <ud_decode+0x630>
+		0x89, 0x1c, 0x24, //                    2 mov    %ebx,(%esp)
+		0x83, 0xe0, 0x0f, //                    5 and    $0xf,%eax
+		0x8d, 0x50, 0x01, //                    8 lea    0x1(%eax),%edx
+		0xd1, 0xea,       //                   11 shr    %edx
+		0x88, 0x55, 0xcc, //                   13 mov    %dl,-0x34(%ebp)
+		0xe8, 0x88, 0xe3, 0xff, 0xff, //       16 call   8048ef0 <inp_next>
+		0x8b, 0x83, 0x58, 0x02, 0x00, 0x00, // 21 mov    0x258(%ebx),%eax
 };
 
 TEST(disassembly)
