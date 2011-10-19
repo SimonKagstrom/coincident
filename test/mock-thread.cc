b10b490ed1c49d7a09895f4ffc467f6f158c36c9
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-10-19 07:23:20 +0200

    unit test: Add expectations on mock

--- test/mock-thread.cc
+++ test/mock-thread.cc
@@ -7,9 +7,15 @@ public:
 	Thread(void (*exitHook)(),
 			int (*fn)(void *), void *arg)
 	{
+		EXPECT_CALL(*this, getRegs())
+				.Times(AtLeast(1))
+				.WillRepeatedly(Return((void *)m_regs))
+				;
 	}
 
 	MOCK_METHOD0(getRegs, void *());
+
+	uint8_t m_regs[8];
 };
 
 #include "../src/thread.cc"
