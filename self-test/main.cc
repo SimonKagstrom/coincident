e0bc3d6bf1e51a2820ffb8ddee48c3af08e8d076
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-10-29 15:50:45 +0200

    self-test: "Refactor" the self-test to make it into a unit test
    This is how coincident is meant to be used anyway. For slightly more
    serious problems though :-)

--- self-test/main.cc
+++ self-test/main.cc
@@ -1,24 +1,98 @@
-#include <coincident/coincident.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <crpcut.hpp>
 #include <stdio.h>
 
-static int fn_a(void *params)
+#include <coincident/coincident.h>
+
+using namespace testing;
+
+
+/*
+ * The code below is not stupid, but VERY stupid.
+ *
+ * However, it's just for testing coincident.
+ */
+int global;
+
+int add_val(int v)
 {
-	printf("fn_a!\n");
-	return 1;
+	global += v;
+
+	return global;
 }
 
-static int fn_b(void *params)
+static int test_race(void *params)
 {
-	printf("fn_b!\n");
-	return 2;
+	global = 1;
+
+	ASSERT_TRUE(global == 1);
+
+	int v = add_val(1);
+	ASSERT_TRUE(v == 2);
+
+	ASSERT_TRUE(v == global);
+
+	return 0;
+}
+
+
+// Same as above, but without races (stack allocated stuff)
+int add_val_non_race(int *src, int v)
+{
+	*src += v;
+
+	return *src;
+}
+
+static int test_non_race(void *params)
+{
+	int src = 1;
+
+	ASSERT_TRUE(src == 1);
+
+	int v = add_val_non_race(&src, 1);
+	ASSERT_TRUE(v == 2);
+
+	ASSERT_TRUE(v == src);
+
+	return 0;
+}
+
+
+TESTSUITE(coincident)
+{
+	TEST(basic_race)
+	{
+		// We use random scheduling here, so this actually affects
+		// the result
+		srand(time(NULL));
+
+		coincident_add_thread(test_race, NULL);
+		coincident_add_thread(test_race, NULL);
+
+		coincident_set_run_limit(10);
+
+		int result = coincident_run();
+		ASSERT_TRUE(result == 0);
+	}
+
+	TEST(basic_non_race)
+	{
+		srand(time(NULL));
+
+		coincident_add_thread(test_non_race, NULL);
+		coincident_add_thread(test_non_race, NULL);
+
+		// Stop after 500 ms
+		coincident_set_time_limit(500);
+
+		int result = coincident_run();
+		ASSERT_TRUE(result == 0);
+	}
 }
 
 int main(int argc, const char *argv[])
 {
-	coincident_add_thread(fn_a, NULL);
-	coincident_add_thread(fn_b, NULL);
-
-	coincident_set_run_limit(2);
-
-	return coincident_run();
+	return crpcut::run(argc, argv);
 }
